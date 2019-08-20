6f9c5f42568a0eca6d5d0b5d2bed78404932fd42
Marty McFly <mirko.maya@live.com>
2019-08-20 14:14:07 -0400

    Buttons are prepending images onto page.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -47,27 +47,28 @@ $(document).ready(function () {
         })
         .then(function(response) {
 
-            console.log(response);
+            // console.log(response);
 
             var responseArray = response.data;
 
-            console.log(responseArray.length);
+            // console.log(responseArray);
 
             for (var j = 0; j < responseArray.length; j++) {
 
+                var imageURL = responseArray[j].images.original_still.url;
 
-                // get images
+                // console.log(imageURL);
 
-                var imageURL = responseArray[i].url;
+                var topicImage = $("<img>");
 
-                console.log(imageURL);
+                topicImage.attr("src", imageURL);
+
+                topicImage.attr("alt", "" + topicName + " image");
+
+                $("#topicImages").prepend(topicImage);
+
 
             }
-
-            // var imageURL = response.data[0];
-
-            // console.log(imageURL);
-
 
 
         })
@@ -77,5 +78,35 @@ $(document).ready(function () {
 
 
 })
+
+
+
+
+// Before you can make any part of your site work, you need to create an array of strings, each one related to a topic that interests you. Save it to a variable called topics.
+
+
+// We chose animals for our theme, but you can make a list to your own liking.
+
+
+
+// Your app should take the topics in this array and create buttons in your HTML.
+
+
+// Try using a loop that appends a button for each string in the array.
+
+
+// When the user clicks on a button, the page should grab 10 static, non-animated gif images from the GIPHY API and place them on the page.
+// When the user clicks one of the still GIPHY images, the gif should animate. If the user clicks the gif again, it should stop playing.
+
+// Under every gif, display its rating (PG, G, so on).
+
+
+// This data is provided by the GIPHY API.
+// Only once you get images displaying with button presses should you move on to the next step.
+
+
+// Add a form to your page that takes a value from a user input box and adds it to your topics array. Then make a function call that takes each topic in the array and remakes the buttons on the page.
+// Deploy your assignment to Github Pages.
+// Rejoice! You just made something really cool.
 
 
